92ed88ea00c23f2b78f6811d5325f49b040727b6
oilcans <64730051+apladi@users.noreply.github.com>
2022-09-15 21:44:30 +1000

    Added files
    Example html files, compile file and source code,.

--- main.c
+++ main.c
@@ -1,222 +1,119 @@
-/* oilscript, the self-centred-named language */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <readline/readline.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#define MAXSIZE 2000
 
-typedef enum {
-    TYPE_ADD, // Addition
-    TYPE_SUB, // Subtraction
-    TYPE_MPY, // Multiplication
-    TYPE_DIV, // Division
-    TYPE_GRT, // Greater than
-    TYPE_LWR, // Lower than
-    TYPE_QTM, // Quotations (UNUSED)
-    TYPE_STR, // String
-    TYPE_INT, // Integer
-    TYPE_SET, // Set (Equals)
-    TYPE_END, // Semi-colon, line end 
-    TYPE_OPR, // Open parenthesis
-    TYPE_CPR, // Close parenthesis
-    TYPE_IDN, // Identifier
-    TYPE_CMA  // Comma
-} type_t;
+int sendf(int sock, char filename[], char type[]) {
+    FILE *file;
+    size_t length;
+    char *sendstr;
+    char *content;
 
-typedef struct {
-    type_t type;
-    char   data[256];
-} token_t;
+    file = fopen(filename, "rb");
 
-typedef struct {
-    int     rank;
-    token_t token;
-} parse_t;
-
-const char *str_type(type_t type) {
-    switch (type) {
-        case  0: return "ADD"; break;
-        case  1: return "SUB"; break;
-        case  2: return "MPY"; break;
-        case  3: return "DIV"; break;
-        case  4: return "GTR"; break;
-        case  5: return "LWR"; break;
-        case  6: return "QTM"; break;
-        case  7: return "STR"; break;
-        case  8: return "INT"; break;
-        case  9: return "SET"; break;
-        case 10: return "END"; break;
-        case 11: return "OPR"; break;
-        case 12: return "CPR"; break;
-        case 13: return "IDN"; break;
-        case 14: return "CMA"; break;
+    if (file == NULL) {
+        printf("[Carlton] Could not open file \"%s\". Are you sure the file exists?\n", filename);
+        send(sock, "HTTP/1.1 500", strlen("HTTP/1.1 500"), 0);
+        return 1;
     }
+    
+    fseek(file, 0, SEEK_END);
+    length  = ftell(file);
+    content = malloc(length);
+    sendstr = malloc(length + 1000);
+    rewind(file);
+    fread(content, length, 1, file);
+    fclose(file);
+
+    if (type == NULL) {
+        strcpy(sendstr, content);
+    } else {
+        strcpy(sendstr, "HTTP/1.1 200 OK\nServer: Carlton\nContent-Type: ");
+        strcat(sendstr, type);
+        strcat(sendstr, "\n\n");
+        strcat(sendstr, content);
+    }
+
+    send(sock, sendstr, strlen(sendstr), 0);
+    return 0;
+}
+
+int sendt(int sock, char input[], char type[]) {
+    char *final;
+
+    final = malloc(strlen(input) + 1000);
+
+    if (type == NULL) {
+        strcpy(final, input);
+    } else {
+        strcpy(final, "HTTP/1.1 200 OK\nServer: Carlton\nContent-Type: ");
+        strcat(final, type);
+        strcat(final, "\n\n");
+        strcat(final, input);
+    }
+
+    if (send(sock, final, strlen(final), 0) == -1) {
+        printf("[Carlton] Could not send text to client.\n");
+        return 1;
+    }
+    
+    return 0;
 }
 
 int main(void) {
-    parse_t parsed[256];
-    token_t tokens[256];
-    token_t token;
-    char *input;
-    int case_for = 0;
-    int counter  = 0;
-    int check    = 0;
+    socklen_t size;
+    time_t current;
+    struct sockaddr_in svradr;
+    char *received = malloc(8192);
+    char *token;
+    int count;
+    int sock;
+    int cxn;
 
-    input = readline("--> ");
+    printf("[Carlton] Starting...\n");
 
-    for (int i = 0; i < strlen(input); ++i) {
-        check = 0;
-        memset(token.data, 0, sizeof(token.data));
+    sock = socket(AF_INET, SOCK_STREAM, 0);
 
-        switch (input[i]) {
-            case '+':
-                token.type    = TYPE_ADD;
-                token.data[0] = '+';
-                token.data[1] = '\0';
-                break;
-            case '-':
-                token.type    = TYPE_SUB;
-                token.data[0] = '-';
-                token.data[1] = '\0';
-                break;
-            case '*':
-                token.type    = TYPE_MPY;
-                token.data[0] = '*';
-                token.data[1] = '\0';
-                break;
-            case '/':
-                token.type    = TYPE_DIV;
-                token.data[0] = '/';
-                token.data[1] = '\0';
-                break;
-            case '>':
-                token.type    = TYPE_GRT;
-                token.data[0] = '>';
-                token.data[1] = '\0';
-                break;
-            case '<':
-                token.type    = TYPE_LWR;
-                token.data[0] = '<';
-                token.data[1] = '\0';
-                break;
-            case '"':
-                token.type = TYPE_STR;
-                case_for   = 0;
-                ++i;
-                for (i; i < strlen(input); ++i) {
-                    if (input[i] == '"') {
-                        tokens[counter] = token;
-                        ++counter;
-                        break;
-                    } else {
-                        token.data[case_for] = input[i];
-                        ++case_for;
-                    }
-                }
-
-                check = 1;
-                break;
-            case '=':
-                token.type    = TYPE_SET;
-                token.data[0] = '=';
-                token.data[1] = '\0';
-                break;
-            case ';':
-                token.type    = TYPE_END;
-                token.data[0] = ';';
-                token.data[1] = '\0';
-                break;
-            case '(':
-                token.type    = TYPE_OPR;
-                token.data[0] = '(';
-                token.data[1] = '\0';
-                break;
-            case ')':
-                token.type    = TYPE_CPR;
-                token.data[0] = ')';
-                token.data[1] = '\0';
-                break;
-            case ',':
-                token.type    = TYPE_CMA;
-                token.data[0] = ',';
-                token.data[1] = '\0';
-                break;
-            default:
-                if (isdigit(input[i])) {
-                    token.type = TYPE_INT;
-                    case_for   = 0;
-
-                    for (i; i < strlen(input); ++i) {
-                        if (!isdigit(input[i])) {
-                            tokens[counter] = token;
-                            ++counter;
-
-                            --i;
-                            break;
-                        } else {
-                            token.data[case_for] = input[i];
-                            ++case_for;
-                        }
-                    }
-
-                    if (i == strlen(input)) {
-                        tokens[counter] = token;
-                        ++counter;
-                    }
-
-                    check = 1;
-                } else if (isalpha(input[i])) {
-                    token.type = TYPE_IDN;
-                    case_for   = 0;
-
-                    for (i; i < strlen(input); ++i) {
-                        if (!isalpha(input[i])) {
-                            tokens[counter] = token;
-                            ++counter;
-
-                            --i;
-                            break;
-                        } else {
-                            token.data[case_for] = input[i];
-                            ++case_for;
-                        }
-                    }
-
-                    if (i == strlen(input)) {
-                        tokens[counter] = token;
-                        ++counter;
-                    }
-
-                    check = 1;
-                } else {
-                    check = 1;
-                }
-
-                break;
-        }
-
-        if (!check) {
-            tokens[counter] = token;
-            ++counter;
-        }    
+    if (sock == -1) {
+        printf("[Carlton] Could not create the socket.\n");
     }
 
-    /* PARSER (BROKEN) */ 
-    // int rank = 0;
+    printf("[Carlton] Socket created.\n");
 
-    // for (int i = 0; i < counter; ++i) {
-    //     if (tokens[i].type == TYPE_END) {
-    //         break;
-    //     }
+    svradr.sin_family      = AF_INET;
+    svradr.sin_addr.s_addr = INADDR_ANY;
+    svradr.sin_port        = htons(1111);
 
-    //     printf("%i|", rank);
-    //     parsed[i].token = tokens[i];
+    if (bind(sock, (struct sockaddr*)&svradr, sizeof(svradr)) == -1) {
+        printf("[Carlton] Could not bind.\n");
+        return 1;
+    }
 
-    //     for (int x = 0; x < rank; ++x) {
-    //         putchar(32);
-    //     }
+    printf("[Carlton] Binded.\n");
+    
+    if (listen(sock, 5) == -1) {
+        printf("[Carlton] Could not listen (on the socket).\n");
+        return 1;
+    }
+    
+    printf("[Carlton] Binded and listening.\n");
 
-    //     printf("%s-%s\n", str_type(parsed[i].token.type), parsed[i].token.data);
-    // }
+    while (sock) {
+        cxn = accept(sock, (struct sockaddr*)&svradr, &size);
+        recv(cxn, received, 8192, 0);
 
-    free(input);
+        token = strtok(received, "GET ");
+        printf("[Carlton] CXN: %s\n", inet_ntoa(svradr.sin_addr));
+
+        if (strcmp(token, "/") == 0) {
+            sendf(cxn, "index.html", "text/html");
+        } else {
+            sendf(cxn, "404.html", "text/html");
+        }
+        
+        close(cxn);
+    }
 }
